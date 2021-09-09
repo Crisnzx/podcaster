652831bc7cab08ccff60dc652185c08efc5ab87b
Cristovao Neto <cristovaon13@gmail.com>
2021-09-08 21:48:16 -0300

    typescript features and axios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,4 +23,16 @@ COMMENTS AND NOTES ABOUT THE LEARNING PROCESS WHEN BUILDING THIS APPLICATION
 
 the pages folder must exists inside of src folder or in the project root.
 
+SPA - Single Page Application
+Requisição dos dados quando o cliente acessar a página pelo lado do cliente. Ruim SEO
+
+SSR - Server Side Rendering
+Requisição dos dados quando o cliente acessar a página pelo lado do servidor. HTML pronto retornado e a página vem
+com todo o seu conteúdo para uma boa indexação
+
+SSG - Static Site Generation
+Requisição dos dados ao back-end feita de tempos em tempos. Geração estática de uma página HTML no servidor do Next que
+será exibida para todos os usuários que acessarem a página, evitando múltiplas requisições ao back-end sem
+necessidade, pois o conteúdo a ser retornado é o mesmo.
+
 */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,61 @@
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
+type Episode = {
+  id: string,
+  title: string,
+  members: string,
+  publishedAt: string,
+  thumbnail: string,
+  url: string,
+  duration: string,
+}
 
-export default function Home() {
+type HomeProps = {
+  episodes: Array<Episode>
+}
+
+export default function Home(props: HomeProps) {
+
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
 }
+// If your page doesn't change too much and you want to have more performance
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await api('episodes', {
+    params: {
+
+    }
+  });
+  const episodes = response.data;
+
+  return {
+    props: {
+      episodes
+    },
+    // One podcast per day
+    revalidate: 60 * 60 * 4,
+  }
+}
+
+
+
+
+
+// If you need to make a new request for every single clien
+/*
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const podcasters = await response.json();
+  return {
+    props: {
+      podcasters
+    }
+  }
+}
+ */
+
